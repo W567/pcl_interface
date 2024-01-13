e4df8846b1c9b64020c5cacaa06fd43a61cfbbe7
wu <wu@jsk.imi.i.u-tokyo.ac.jp>
2024-01-13 18:18:55 +0900

    [pc2palmPublisher] maybe crop point cloud with no points under desk directly after prediction is better? delete cropping in pc2palmPublisher

--- scripts/pc2palmPublisher.py
+++ scripts/pc2palmPublisher.py
@@ -16,7 +16,6 @@ class pc2palmPublisher(pcPubBase):
     def __init__(self):
         super().__init__()
         self.base_frame = rospy.get_param('~base_frame', 'base')
-        self.desk_frame = rospy.get_param('~desk_frame', 'desk_frame')
 
         # tf from base to palm
         self.palm_rot = np.eye(4)
@@ -24,24 +23,10 @@ class pc2palmPublisher(pcPubBase):
 
         self.br = tf.TransformBroadcaster()
 
-        min_inf = np.finfo(np.float32).min
-        max_inf = np.finfo(np.float32).max
-        self.min_bound = np.array([min_inf, 0.0, min_inf])
-        self.max_bound = np.array([max_inf, max_inf, max_inf])
-
     def pcd_trans(self, pcd):
-
-        (desk_t, desk_r) = self.tf_listener.lookupTransform(self.base_frame, self.desk_frame,
-                                                            rospy.Time(0))
-        desk_z = desk_t[2]
-
         # pcd under base coordinate system
         center = -pcd.get_center()
-        obj_min = pcd.get_min_bound()[2]
-        if obj_min < desk_z:
-            center[2] = -desk_z
-        else:
-            center[2] = -obj_min
+        center[2] = -pcd.get_min_bound()[2]
         # tf from object_bot to base
         trans = np.eye(4)
         trans[:3, 3] = center
@@ -49,22 +34,6 @@ class pc2palmPublisher(pcPubBase):
         trans = self.palm_rot @ trans
         # pcd under object_bot/palm coordinate system
         pcd = deepcopy(pcd).transform(trans)
-
-        # pcd has been transformed to palm coordinate system
-        points_np = np.asarray(pcd.points)
-        mask = np.all((self.min_bound <= points_np) & (points_np <= self.max_bound), axis=1)
-        cropped_points_np = points_np[mask]
-        pcd.points = o3d.utility.Vector3dVector(cropped_points_np)
-        if pcd.has_colors:
-            colors_np = np.asarray(pcd.colors)
-            if colors_np.size != 0:
-                cropped_colors_np = colors_np[mask]
-                pcd.colors = o3d.utility.Vector3dVector(cropped_colors_np)
-        if pcd.has_normals:
-            normals_np = np.asarray(pcd.normals)
-            if normals_np.size != 0:
-                cropped_normals_np = normals_np[mask]
-                pcd.normals = o3d.utility.Vector3dVector(cropped_normals_np)
 
         # tf from base to object_bot
         trans = np.linalg.inv(trans)
